38ef712e6d39287db06fc077f54989062820e218
CHia <dozeuto@gmail.com>
2023-08-10 16:04:41 +0800

    Update chapter3_5_2.py

--- chapter3/chapter3_5_2.py
+++ chapter3/chapter3_5_2.py
@@ -1,14 +1,14 @@
-class StrKeyDict0(dict):
+import collections
+
+
+class StrKeyDict0(collections.UserDict):
     def __missing__(self, key):
         if isinstance(key, str):
             raise KeyError(key)
         return self[str(key)]
 
-    def get(self, key, default=None):
-        try:
-            return self[key]
-        except KeyError:
-            return default
-
     def __contains__(self, key):
-        return key in self.keys() or str(key) in self.keys()
+        return str(key) in self.data
+
+    def __setitem__(self, key, value):
+        self.data[str(key)] = value
