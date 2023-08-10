a5f351c7362e1916107e2a0a3e2352e6e709c1f1
CHia <dozeuto@gmail.com>
2023-08-10 15:09:06 +0800

    update chapter3

---
+++ chapter3/chapter3_2_1.py
@@ -0,0 +1,15 @@
+dial_codes = [
+    (880, 'Bangladesh'),
+    (55, 'Brazil'),
+    (86, 'China'),
+    (91, 'India'),
+    (62, 'Indonesia'),
+    (81, 'Japan'),
+    (234, 'Nigeria'),
+    (92, 'Pakistan'),
+    (7, 'Russia'),
+    (1, 'United States')
+]
+
+country_dial = {country: code for code, country in dial_codes}
+print(country_dial)
---
+++ chapter3/creator.py
@@ -0,0 +1,26 @@
+from collections import OrderedDict
+
+
+def get_creators(record: dict) -> list:
+    match record:
+        case {'type': 'book', 'api': 2, 'authors': [*names]}:
+            return names
+        case {'type': 'book', 'api': 1, 'author': name}:
+            return [name]
+        case {'type': 'book'}:
+            raise ValueError(f"Invalid 'book' record: {record!r}")
+        case {'type': 'movie', 'director': name}:
+            return [name]
+        case _:
+            raise ValueError(f'Invalid record: {record!r}')
+
+
+b1 = dict(api=1, author='Douglas Hofstadter',
+          type='book', title='Gödel, Escher, Bach')
+
+print(get_creators(b1))
+
+b2 = OrderedDict(api=2, type='book', title='Python in a Nutshell', authors='Martelli Ravenscroft Holden'.split())
+print(get_creators(b2))
+
+print(get_creators({'type': 'book', 'page': 770}))
