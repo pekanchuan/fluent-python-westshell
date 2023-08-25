4dfeaf6bcbe6285a6e5237e4dea65e4850caa9d3
CHia <dozeuto@gmail.com>
2023-08-25 16:33:44 +0800

    Create fibo_demo.py

---
+++ chapter9/fibo_demo.py
@@ -0,0 +1,14 @@
+from clockdeco import clock
+import functools
+
+
+@functools.cache
+@clock
+def fibonacci(n):
+    if n < 2:
+        return n
+    return fibonacci(n - 2) + fibonacci(n - 1)
+
+
+if __name__ == "__main__":
+    print(fibonacci(6))
