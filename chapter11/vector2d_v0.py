ed0ac9dac23088d7e43410a463addc49709f084f
CHia <dozeuto@gmail.com>
2023-08-28 15:26:42 +0800

    Update vector2d_v0.py

--- chapter11/vector2d_v0.py
+++ chapter11/vector2d_v0.py
@@ -6,8 +6,16 @@ class Vector2d:
     typecode = 'd'
 
     def __init__(self, x, y):
-        self.x = float(x)
-        self.y = float(y)
+        self.__x = float(x)
+        self.__y = float(y)
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
 
     def __iter__(self):
         return (i for i in (self.x, self.y))
@@ -32,7 +40,46 @@ class Vector2d:
     def __bool__(self):
         return bool(abs(self))
 
+    def __format__(self, format_spec=''):
+        if format_spec.endswith('p'):
+            format_spec = format_spec[:-1]
+            coords = (abs(self), self.angle())
+            outer_fmt = '<{}, {}>'
+        else:
+            coords = self
+            outer_fmt = '({], {})'
+        components = (format(c, format_spec) for c in coords)
+        return outer_fmt.format(*components)
+
+    def angle(self):
+        return math.atan2(self.y, self.x)
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+    @classmethod
+    def frombytes(cls, octets):
+        typecode = chr(octets[0])
+        memv = memoryview(octets[1:]).cast(typecode)
+        return cls(*memv)
+
 
 if __name__ == '__main__':
     v = Vector2d(3, 4)
-    print(bytes(v))
+    print(hash(v))
+
+
+    class Demo:
+        @classmethod
+        def klassmeth(*args):
+            return args
+
+        @staticmethod
+        def statmeth(*args):
+            return args
+
+
+    print(Demo.klassmeth())
+    print(Demo.klassmeth('spam'))
+    print(Demo.statmeth())
+    print(Demo.statmeth('spam'))
