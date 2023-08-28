3cc8940c4829376056ff421899f9cae4d686c02d
CHia <dozeuto@gmail.com>
2023-08-28 11:30:26 +0800

    Create order_func.py

---
+++ chapter10/order_func.py
@@ -0,0 +1,72 @@
+from collections.abc import Sequence
+from dataclasses import dataclass
+from decimal import Decimal
+from typing import Optional, Callable, NamedTuple
+
+
+class Customer(NamedTuple):
+    name: str
+    fidelity: int
+
+
+class LineItem(NamedTuple):
+    product: str
+    quantity: int
+    price: Decimal
+
+    def total(self) -> Decimal:
+        return self.quantity * self.price
+
+
+@dataclass(frozen=True)
+class Order:
+    customer: Customer
+    cart: Sequence[LineItem]
+    promotion: Optional[Callable[['Order'], Decimal]] = None
+
+    def total(self):
+        totals = (item.total() for item in self.cart)
+        return sum(totals, start=Decimal(0))
+
+    def due(self):
+        if self.promotion is None:
+            discount = Decimal(0)
+        else:
+            discount = self.promotion(self)
+        return self.total() - discount
+
+    def __repr__(self):
+        return f'<Order total: {self.total():.2f} due: {self.due():.2f}>'
+
+
+def fidelity_promo(order: Order):
+    if order.customer.fidelity >= 1000:
+        return order.total() * Decimal('0.05')
+    return Decimal(0)
+
+
+def bulk_item_promo(order: Order):
+    discount = Decimal(0)
+    for item in order.cart:
+        if item.quantity >= 20:
+            discount += item.total() * Decimal('0.1')
+    return discount
+
+
+def large_order_promo(order: Order):
+    distinct_items = {item.product for item in order.cart}
+    if len(distinct_items) >= 10:
+        return order.total() * Decimal('0.07')
+    return Decimal(0)
+
+
+if __name__ == '__main__':
+    joe = Customer('John Doe', 0)
+    ann = Customer('Ann Smith', 1100)
+
+    cart = [
+        LineItem('banana', 4, Decimal('.5')),
+        LineItem('apple', 10, Decimal('1.5')),
+        LineItem('watermelon', 5, Decimal(5)),
+    ]
+    print(Order(ann, cart, fidelity_promo))
