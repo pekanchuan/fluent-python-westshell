b999f17029b6df9af83e4263548bcccf7d757c76
CHia <dozeuto@gmail.com>
2023-08-30 14:19:34 +0800

    Create double_protocol.py

---
+++ chapter13/double_protocol.py
@@ -0,0 +1,15 @@
+from typing import TypeVar, Protocol
+
+T = TypeVar('T')
+
+
+class Repeatable(Protocol):
+    def __mul__(self: T, repeat_count: int) -> T:
+        ...
+
+
+RT = TypeVar('RT', bound=Repeatable)
+
+
+def double(x: RT) -> RT:
+    return x * 2
