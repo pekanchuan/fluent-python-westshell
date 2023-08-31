82376ca46803129205ec6eb78b2bd2798887f73c
CHia <dozeuto@gmail.com>
2023-08-31 16:19:00 +0800

    Update books.py

--- chapter15/books.py
+++ chapter15/books.py
@@ -8,10 +8,25 @@ class BookDict(TypedDict):
     pagecount: int
 
 
+AUTHOR_ELEMENT = '<AUTHOR>{}</AUTHOR>'
+
+
+def to_xml(book: BookDict) -> str:
+    elements: list[str] = []
+    for key, value in book.items():
+        if isinstance(value, list):
+            elements.extend(AUTHOR_ELEMENT.format(n) for n in value)
+        else:
+            tag = key.upper()
+            elements.append(f'<{tag}>{value}</{tag}>')
+    xml = '\n\t'.join(elements)
+    return f'<BOOK>\n\t{xml}\n</BOOK>'
+
+
 if __name__ == '__main__':
     pp = BookDict(title='Programming Pearls',
-                  authors='Jon Bentley',
+                  authors=['Jon Bentley'],
                   isbn='0201657880',
                   pagecount=256)
 
-    print(pp)
+    print(to_xml(pp))
