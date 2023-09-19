db63a50a615b3f0bcfac64e5ccca7015a27071fb
CHia <dozeuto@gmail.com>
2023-09-19 17:10:49 +0800

    Create bulkfood_v5.py

---
+++ 23/bulkfood_v5.py
@@ -0,0 +1,19 @@
+import model_v5 as model
+
+
+class LineItem:
+    description = model.NonBlank()
+    weight = model.Quantity()
+    price = model.Quantity()
+
+    def __init__(self, description, weight, price):
+        self.description = description
+        self.weight = weight
+        self.price = price
+
+    def subtotal(self):
+        return self.weight * self.price
+
+
+if __name__ == '__main__':
+    truffle = LineItem('White truffle', 100, 10)
