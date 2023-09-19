61f45effe59cab7d0274567f096908709c07b877
CHia <dozeuto@gmail.com>
2023-09-19 16:53:50 +0800

    Create model_v4c.py

---
+++ 23/model_v4c.py
@@ -0,0 +1,11 @@
+class Quantity:
+
+    def __set_name__(self, owner, name):
+        self.storage_name = name
+
+    def __set__(self, instance, value):
+        if value > 0:
+            instance.__dict__[self.storage_name] = value
+        else:
+            msg = f'{self.storage_name} must be > 0'
+            raise ValueError(msg)
